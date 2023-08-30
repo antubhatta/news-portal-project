18970b8b511da2285b0a09a79ccde0d1df0d9385
antubhatta <antubhatta09@gmail.com>
2023-08-30 23:55:53 +0600

    news catagory added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('hi i am')
+const loadCategory=async()=>{
+    const res= await fetch(`https://openapi.programming-hero.com/api/news/categories`)
+    const data= await res.json()
+    const news= data.data.news_category
+    // console.log(news)
+
+    displayCategory(news)
+
+}
+const displayCategory=news=>{
+    console.log(news)
+    news=news.slice(0,3)
+    const tabContainer= document.getElementById('tab-container')
+    news.forEach(data=>{
+        const div=document.createElement('div')
+        div.innerHTML=`
+        <a class="tab text-2xl">${data.category_name}</a>
+        `
+        tabContainer.appendChild(div)
+    })
+    
+}
+loadCategory()
